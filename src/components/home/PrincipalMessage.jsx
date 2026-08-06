64a1de6aa050bacd019fd9fc31b89a759059b7ea
Ankit-nehra <ankitnehra7000@gmail.com>
2026-08-06 17:48:02 +0530

    Update PrincipalMessage.jsx

--- src/components/home/PrincipalMessage.jsx
+++ src/components/home/PrincipalMessage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import principalImage from "../../assets/images/principal.jpeg";
+import principalImage from "../../assets/images/photo.jpg";
 
 function PrincipalMessage() {
   return (
